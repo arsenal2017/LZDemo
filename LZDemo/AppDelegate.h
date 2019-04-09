d0679d81ac0bd54da6131cf441c595bc4d294463
arsenal2017 <1274163421@qq.com>
2019-04-09 16:58:32 +0800

    init project

---
+++ LZDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZDemo
+//
+//  Created by 赵子远 on 2019/4/9.
+//  Copyright © 2019 com.siyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZDemo/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  LZDemo
+//
+//  Created by 赵子远 on 2019/4/9.
+//  Copyright © 2019 com.siyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <LZNetworking.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
